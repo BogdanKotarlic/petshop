9f2b289df6692523d4c500e3076d42d0db72333e
BogdanKotarlic <31574433+BogdanKotarlic@users.noreply.github.com>
2021-10-15 22:10:41 +0200

    home update

--- petshop-frontend/src/core/Home.js
+++ petshop-frontend/src/core/Home.js
@@ -1,60 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import Layout from './Layout';
-import { getProducts } from './apiCore';
-import Card from './Card';
-import Search from './Search';
+import React, { Fragment } from 'react';
+import Menu from './Menu';
 
 const Home = () => {
-    const [productsBySell, setProductsBySell] = useState([]);
-    const [productsByArrival, setProductsByArrival] = useState([]);
-    const [error, setError] = useState(false);
 
-    const loadProductsBySell = () => {
-        getProducts('sold').then(data => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setProductsBySell(data);
-            }
-        })
+    const coloredLine = () => (
+        <hr style={{color: '#white', backgroundColor: 'white', height: 1, width: '75%'}} />
+    );
+
+    const homeScreen = () => {
+        return (
+            <Fragment>
+                <div className="container-fluid padding">
+                    <div className="row welcome text-center">
+                        <div className="col-12">
+                            <h1 className="display-4">WELCOME TO PET SHOP</h1>
+                        </div>
+                        <div className="col-12">
+                            <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        </div>
+                    </div>
+                </div>
+                {coloredLine()}
+                <div className="container-fluid padding">
+                    <div className="row text-center padding">
+                        <div className="col-xs-12 col-sm-6 col-md-4">
+                            <img src="/images/toys.png" alt="toys" style={{width: '45px', height: '45px', marginTop: '2rem', marginBottom: '1rem'}} />
+                            <h3>TOYS</h3>
+                            <p>You can buy toy for your pet on this online shop</p>
+                        </div>
+                        <div className="col-xs-12 col-sm-6 col-md-4">
+                            <img src="/images/dog-food.png" alt="toys" style={{width: '45px', height: '45px', marginTop: '2rem', marginBottom: '1rem'}} />
+                            <h3>FOOD</h3>
+                            <p>You can buy food for your pet on this online shop</p>
+                        </div>
+                        <div className="col-sm-12 col-md-4">
+                            <img src="/images/brush.png" alt="toys" style={{width: '45px', height: '45px', marginTop: '2rem', marginBottom: '1rem'}} />
+                            <h3>HYGIENE</h3>
+                            <p>You can buy everything for hygiene on this online shop</p>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        );
     };
-
-    const loadProductsByArrival = () => {
-        getProducts('createdAt').then(data => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setProductsByArrival(data);
-            }
-        })
-    };
-
-    useEffect(() => {
-        loadProductsByArrival();
-        loadProductsBySell();
-    }, []);
 
     return (
-        <Layout title="Home Page" description="Node React Webshop App" className="container-fluid">
-            <Search />
-            <h2 className="mb-4">New Arrivals</h2>
-            <div className="row">
-                {productsByArrival.map((product, i) => (
-                    <div key={i} className="col-4 mb-3">
-                        <Card product={product} />
-                    </div>
-                ))}
-            </div>
-
-            <h2 className="mb-4">Best Sellers</h2>
-            <div className="row">
-                {productsBySell.map((product, i) => (
-                    <div key={i} className="col-4 mb-3">
-                        <Card product={product} />
-                    </div>
-                ))}
-            </div>
-        </Layout>
+        <Fragment>
+            <Menu />
+            {homeScreen()}
+        </Fragment>
     );
 };
 
